e3849512f9d7443b4529f02221e505b098d25051
Sean Levorse <smlevorse@msn.com>
2016-10-04 22:00:30 -0400

    Created abstract class for DirectoryObjects that DirectoryNode and FileNode will inherit from.

---
+++ DiskMapper/DirectoryNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DiskMapper
+{
+    class DirectoryNode : DirectoryObject
+    {
+        #region Fields
+
+        private long _size;
+        private string _path;
+        private DirectoryObject _parent;
+        private DirectoryObject[] _children;
+
+        #endregion
+
+        #region Properties
+
+        public override DirectoryObject Parent { get { return _parent; } }
+
+        public override long Size { get { return _size; } }
+
+        public override string Path { get { return _path; } }
+        
+        public DirectoryObject[] Children { get { return _children; } }
+
+        #endregion
+
+        #region Constructors
+
+        public DirectoryNode(DirectoryObject parent, string objectName)
+        {
+            _path = parent.Path + objectName;
+            _size = 0;
+            _parent = parent;
+
+            
+        }
+
+        #endregion
+
+        protected override void UpdateSize(long size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DiskMapper/DirectoryObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskMapper
+{
+    abstract class DirectoryObject
+    {
+        /// <summary>
+        /// The size in bytes of the object
+        /// </summary>
+        public abstract long Size { get; }
+
+        /// <summary>
+        /// The path to the object
+        /// </summary>
+        public abstract string Path { get; }
+
+        /// <summary>
+        /// The directory this directory object is located in
+        /// </summary>
+        public abstract DirectoryObject Parent { get; }
+        
+        /// <summary>
+        /// Sets the size of this directory object 
+        /// </summary>
+        /// <param name="size">The number of bytes this directory object takes up</param>
+        protected abstract void UpdateSize(long size);
+        
+    }
+}
